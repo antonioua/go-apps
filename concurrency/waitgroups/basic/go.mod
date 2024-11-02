21613d3de6cb69b5c6d82ef260687f5e95021401
antonioua <xmananton@gmail.com>
2024-11-02 20:38:00 +0200

    add more examples

---
+++ concurrency/waitgroups/basic/go.mod
@@ -0,0 +1,3 @@
+module github.com/antonioua/concurrency/waitgroups/basic
+
+go 1.23.2
---
+++ concurrency/waitgroups/basic/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	defer fmt.Println("Mission completed!")
+
+	evilNinjas := []string{"Ninja Golang", "Ninja Python", "Ninja Ruby", "Ninja JavaScript"}
+	wg.Add(len(evilNinjas))
+
+	for _, evilNinja := range evilNinjas {
+		go attack(evilNinja, &wg)
+	}
+	wg.Wait()
+}
+
+func attack(target string, wg *sync.WaitGroup) {
+	fmt.Println("Attacking ", target)
+	wg.Done()
+}
